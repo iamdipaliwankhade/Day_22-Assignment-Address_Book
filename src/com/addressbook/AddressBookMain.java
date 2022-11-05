cfbc8c3d55e181855a5f2f5e17b8c2828d7ef646
iamdipaliwankhade <iamdipaliwankhade@gmail.com>
2022-11-05 19:04:43 +0530

    [Dipali] Added: add contact

--- src/com/addressbook/AddressBookMain.java
+++ src/com/addressbook/AddressBookMain.java
@@ -1,4 +1,7 @@
 package com.addressbook;
+
+import java.util.Scanner;
+
 /**
  * 
  * @author Dipali
@@ -8,9 +11,26 @@ package com.addressbook;
 public class AddressBookMain {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program");
-
+		Scanner sc = new Scanner(System.in);
 		AddressBook addressBook = new AddressBook();
 
-	}
+		while (true) {
+			System.out.println("0.Exit \n1.Add Contact \n2.Display Contact ");
+			int ch = sc.nextInt();
+			switch (ch) {
+			case 0:
+				System.exit(0);
+				break;
+			case 1:
+				addressBook.addContact();
+				break;
+			case 2:
+				addressBook.displayContact();
+				break;
+			default:
+				System.out.println("Invalid Input");
+			}
+		}
 
+	}
 }
